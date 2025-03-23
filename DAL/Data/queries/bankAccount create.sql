ebf6021c93211a3369a6458993a8fe55a8d391d8
אוטמזגין יהודית < @mbjcomp.local>
2025-03-23 20:52:36 +0200

    adding bankAccount table\n\nAdd BankAccount table with non-nullable columns\n\nA new table named BankAccount has been added to the database schema. This table includes four columns: AccountID, BranchNo, AccountNo, and BillingAccount. The AccountID column is defined as the primary key and cannot be null. The other columns (BranchNo, AccountNo, and BillingAccount) are also defined as not null.

---
+++ DAL/Data/queries/bankAccount create.sql
@@ -0,0 +1,16 @@
+﻿-- Create BankAccount table
+CREATE TABLE BankAccount (
+    AccountID INT PRIMARY KEY NOT NULL,
+    BranchNo NVARCHAR(50) NOT NULL,
+    AccountNo NVARCHAR(50) NOT NULL,
+    BillingAccount NVARCHAR(50) NOT NULL
+);
+
+-- Create Worker table
+CREATE TABLE Worker (
+    WorkerID INT PRIMARY KEY NOT NULL,
+    FirstName NVARCHAR(50) NOT NULL,
+    LastName NVARCHAR(50) NOT NULL,
+    AccountID INT NOT NULL,
+    FOREIGN KEY (AccountID) REFERENCES BankAccount(AccountID)
+);
