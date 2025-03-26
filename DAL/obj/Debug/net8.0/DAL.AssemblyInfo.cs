23cfb4aaf3d427e248a14b30aa6fb43c678c61fb
סמן טוב רבקה <Hand3D23@mbjcomp.org.il>
2025-03-26 16:00:42 +0200

    add models according to DB

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15379112c2a91fa389df806fb3e072201a831c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0312d68181e5671a41525f1cbaf9b74494352a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
