e52c1e69069dd0e4089fc7b5742698b01a6a2a6c
סמן טוב רבקה <Hand3D23@mbjcomp.org.il>
2025-04-21 20:21:54 +0300

    Implementing the interface IAppointmentRepo

--- DAL/Api/IAppointmentRepo.cs
+++ DAL/Api/IAppointmentRepo.cs
@@ -11,20 +11,20 @@ namespace DAL.Api
     {
         // its so big we dont need all of it, why you did it?
         //Task<List<Appointment>> GetAllAppointments();
+        Task AddAppointment(Appointment appointment);
+        Task DeleteAppointment(int id);
         Task<List<Appointment>> GetAppointmentsByPatientId(int patientId);
 
         //Task<Appointment> GetAppointmentById(int id);
-        Task AddAppointment(Appointment appointment);
+
         Task UpdateAppointment(Appointment appointment);
-        Task DeleteAppointment(int id);
+        
 
         // פונקציות נוספות לפי הצורך:
-        Task<List<Appointment>> GetAppointmentsByPatientIdAndDate(int patientId, DateTime date);
-        Task<Appointment> GetAppointmentsByPatientIdAndDate(int patientId, DateTime date, string therapis);
-
-        Task<List<Appointment>> GetAppointmentsByTherapist(string therapist);
-        Task<List<Appointment>> GetAppointmentsByTherapistAndDate(string therapist_name, DateTime date);
-        Task<List<Appointment>> GetAppointmentsByTherapistId(string therapist_name);
-        Task<bool> IsTimeSlotAvailable(DateTime date, TimeSpan time, int employeeId);
+        Task<List<Appointment>> GetAppointmentsByPatientIdAndDate(int patientId, DateOnly date);
+        Task<Appointment> GetAppointmentsByPatientAndThetapistIdIdAndDate(int patientId, DateOnly date, int therapistId);
+        Task<List<Appointment>> GetAppointmentsByTherapistIdAndDate(int therapistId, DateOnly date);
+        Task<List<Appointment>> GetAppointmentsByTherapistId(int therapistId);
+        Task<bool> IsTimeSlotAvailable(DateOnly date, TimeOnly time, int therapistId);
     }
 }
--- DAL/Services/AppointmentRepo.cs
+++ DAL/Services/AppointmentRepo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DAL.Api;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,87 @@ using System.Threading.Tasks;
 
 namespace DAL.Services
 {
-    internal class AppointmentRepo
+    internal class AppointmentRepo : IAppointmentRepo
     {
+        private readonly DB_Manager _context;
+        public AppointmentRepo(DB_Manager context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task AddAppointment(Appointment appointment)
+        {
+            if (appointment == null)
+                throw new ArgumentNullException(nameof(appointment), "Appointment cannot be null.");
+
+            _context.Appointments.Add(appointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment == null)
+                throw new KeyNotFoundException($"Appointment with ID {id} not found.");
+
+            _context.Appointments.Remove(appointment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsByPatientId(int patientId)
+        {
+
+            return await _context.Appointments
+                .Where(a => a.PatientId == patientId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsByPatientIdAndDate(int patientId, DateOnly date)
+        {
+            return await _context.Appointments
+                .Where(a => a.PatientId == patientId && a.AppointmentDate == date)
+                .ToListAsync();
+        }
+
+        public async Task<Appointment> GetAppointmentsByPatientAndThetapistIdIdAndDate(int patientId, DateOnly date, int therapistId)
+        {
+            return await _context.Appointments
+                .Where(a => a.PatientId == patientId && a.AppointmentDate == date)
+                .FirstOrDefaultAsync();
+        }
+
+
+        public async Task<List<Appointment>> GetAppointmentsByTherapistIdAndDate(int therapistId, DateOnly date)
+        {
+            return await _context.Appointments
+                .Where(a => a.TherapistId == therapistId && a.AppointmentDate == date)
+                .ToListAsync();
+        }
+
+        public async Task<List<Appointment>> GetAppointmentsByTherapistId(int therapistId)
+        {
+            return await _context.Appointments
+                .Where(a => a.TherapistId == therapistId)
+                .ToListAsync();
+        }
+
+        public async Task<bool> IsTimeSlotAvailable(DateOnly date, TimeOnly time, int therapistId)
+        {
+            return !await _context.Appointments
+                .AnyAsync(a => a.TherapistId == therapistId && a.AppointmentDate == date && a.AppointmentTime == time);
+        }
+
+        public async Task UpdateAppointment(Appointment appointment)
+        {
+            if (appointment == null)
+                throw new ArgumentNullException(nameof(appointment), "Appointment cannot be null.");
+
+            var existingAppointment = await _context.Appointments.FindAsync(appointment.AppointmentId);
+            if (existingAppointment == null)
+                throw new KeyNotFoundException($"Appointment with ID {appointment.AppointmentId} not found.");
+
+            _context.Entry(existingAppointment).CurrentValues.SetValues(appointment);
+            await _context.SaveChangesAsync();
+        }
     }
 }
