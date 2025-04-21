94119fd3fbadfe8a979d030b017ffd07f5871a34
YehditOutmazgin <Hand3D3@mbjcomp.org.il>
2025-04-21 18:48:00 +0300

    add functions to ITherapistRepo

--- DAL/Api/ITherapistRepo.cs
+++ DAL/Api/ITherapistRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace DAL.Api
 {
     internal interface ITherapistRepo
     {
+        Task<Therapist> GetTherapistById(int id);
+
+        Task AddTherapist(Therapist therapist);
+
+        Task UpdateTherapist(Therapist therapist);
+
+        Task DeleteTherapist(int id);
+        //
+        Task<List<Therapist>> GetAllTherapists();
     }
 }
